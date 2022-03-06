e7e98f017ef377fc3aa3ff0417bf6d0c1424aa53
Frank Pena <frankrosario1998@gmail.com>
2022-03-05 22:25:38 -0400

    nested routes nested

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -15,32 +15,35 @@ export const Navigation = () => {
             <img src={logo} alt="logo" />
 
             <ul>
-              {routes.map(({to,name})=>(
-                <li
-                key={to}
-                >
-                  
-                  <CustomLink
-                  to={to}
-                  > {name}</CustomLink>
+              {routes.map(({ to, name }) => (
+                <li key={to}>
+                  <CustomLink to={to}> {name}</CustomLink>
                 </li>
               ))}
             </ul>
           </nav>
 
           <Routes>
-            {routes.map(route=>(
-            <Route 
-            key={route.name} 
-            path={route.path} 
-            element={<route.Component/>}  />
+            {routes.map((route) => (
+              <Route
+                key={route.name}
+                path={route.path}
+                element={<route.Component />}
+              >
+                {route.children &&
+                  route.children.length > 0 &&
+                  route.children.map((child) => {
+                    console.log(child)
+                    return (
+                    <Route key={child.path} path={route.path} element={<child.Component/>} ></Route>
+                  )})}
+              </Route>
             ))}
-           
-            <Route path="/*" element={<Navigate to={routes[0].to}/>}  />
+
+            <Route path="/*" element={<Navigate to={routes[0].to} />} />
           </Routes>
         </div>
       </BrowserRouter>
-      ,
     </Suspense>
   );
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,34 +1,52 @@
 import { lazy } from "react";
+import { LazyPage1, LazyPage2, LazyPage3, NoLazy } from "../01-lazyload/pages";
 
 
-interface Route{
+interface RouteBase{
     to:string,
     path:string,
     Component:React.LazyExoticComponent<() => JSX.Element>| (()=>JSX.Element),
     name:string
 }
 
-const Lazy1 = lazy(()=>import(/*webpackChunkName: "LazyPage1"*/'../01-lazyload/pages/LazyPage1'))
-const Lazy2 = lazy(()=>import(/*webpackChunkName: "LazyPage2"*/'../01-lazyload/pages/LazyPage2'))
-const Lazy3 = lazy(()=>import(/*webpackChunkName: "LazyPage3"*/'../01-lazyload/pages/LazyPage3'))
+interface Route extends RouteBase{
+    children?:RouteBase[]
+}
+
+const LazyLayout = lazy(()=>import(/*webpackChunkName: "LazyLayout"*/'../01-lazyload/layout/LazyLayout'))
 
 export const routes:Route[] = [
     {
-        to:'/lazy1',
-        path:'lazy1',
-        Component:Lazy1,
-        name:'Lazy-1'
+        to:'/lazyload',
+        path:'/lazyload/*',
+        Component:LazyLayout,
+        name:'LazyLayout- Dash',
+        children:[
+            {
+                to:'/lazy1',
+                path:'lazy1',
+                Component:LazyPage1,
+                name:'Lazy1',
+            },
+            {
+                to:'/lazy2',
+                path:'lazy2',
+                Component:LazyPage2,
+                name:'Lazy2',
+            },
+            {
+                to:'/lazy3',
+                path:'lazy3',
+                Component:LazyPage3,
+                name:'Lazy4',
+            },
+        ]
     },
     {
-        to:'/lazy2',
-        path:'lazy2',
-        Component:Lazy2,
-        name:'Lazy-2'
+        to:'/no-lazy',
+        path:'no-lazy',
+        Component:NoLazy,
+        name:'No Lazy'
     },
-    {
-        to:'/lazy3',
-        path:'lazy3',
-        Component:Lazy3,
-        name:'Lazy-3'
-    },
+ 
 ]
